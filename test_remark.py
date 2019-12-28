d588ae2c568eae49b6845e4911e4495d2eb6b42f
Brooks Kindle <brookskindle+git@gmail.com>
2019-12-27 21:27:27 -0800

    Remove html tests
    This use case is already covered by Pelican itself

--- test_remark.py
+++ test_remark.py
@@ -17,19 +17,3 @@ def test_links_not_in_link_form_are_preserved():
     content = "{static}/images/image.png"
     replaced = remark.replace_internal_links(content)
     assert content == replaced
-
-
-# TODO: html should already be handled by upstream Pelican rendering - would
-# need an integration test for that
-@pytest.mark.skip()
-def test_html_images_are_replaced():
-    content = "<img alt='picture' src='{static}/images/git-status.png'></img>"
-    replaced = remark.replace_internal_links(content)
-    assert content != replaced
-
-
-@pytest.mark.skip()
-def test_html_links_are_replaced():
-    content = "<a src='{static}/images/git-status.png'>Click here</a>"
-    replaced = remark.replace_internal_links(content)
-    assert content != replaced
